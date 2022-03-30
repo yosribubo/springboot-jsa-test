0672eb51416167fa3cc0dd6ea5d8e6c851c41890
yosribubo <yosribubo@gmail.com>
2022-03-30 17:59:24 +0900

    first commit

---
+++ src/main/java/com/example/controller/ControllerDemo.java
@@ -0,0 +1,11 @@
+package com.example.controller;
+import org.springframework.stereotype.Controller;  
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class ControllerDemo {
+    @RequestMapping("/")
+    public String home()
+    {
+        return "home.jsp";
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into userdata values(101, 'Tom');
+insert into userdata values(102, 'Andrew');
+insert into userdata values(103, 'Tony');
